bb579a9d5a4201fdcad3a932038dd203582313bb
Andrea Boccone <andreaboccone33@gmail.com>
2025-05-01 12:31:15 +0200

    running project via task

--- lib/loggers.ts
+++ lib/loggers.ts
@@ -3,11 +3,11 @@ import { green, red, yellow, cyan } from "../deps.ts";
 
 export class Log {
     static info(...args: unknown[]) {
-        console.log(cyan("ℹ️"), ...args);
+        console.log(cyan("ℹ️ "), ...args);
     }
 
     static success(...args: unknown[]) {
-        console.log(green("✅"), ...args);
+        console.log(green("✅ "), ...args);
     }
 
     static warn(...args: unknown[]) {
